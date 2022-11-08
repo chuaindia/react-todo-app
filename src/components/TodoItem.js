3bd8a2ba9be2879fadc44b9251e24138bf9cea36
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-08 19:18:40 +0530

    Add linter files

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,28 +1,29 @@
-import React from "react"
-import styles from "./TodoItem.module.css"
+import React from 'react';
+import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
-    handleEditing = () => {
-        console.log("edit mode activated")
-      }
-    render() {
+  handleEditing = () => {
+    console.log('edit mode activated');
+  };
+
+  render() {
     return (
-        <li className={styles.item}>
-          <div onDoubleClick={this.handleEditing}>...</div>
-          <input
-            type="checkbox"
-            className={styles.checkbox}
-            checked={this.props.todo.completed}
-            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-          />
-          <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-            Delete
-          </button>
-          {this.props.todo.title}
-          <input type="text" className={styles.textInput} />
-        </li>
-      )
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing}>...</div>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        {this.props.todo.title}
+        <input type="text" className={styles.textInput} />
+      </li>
+    );
   }
 }
 
-export default TodoItem
+export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
-//component file
-import TodoContainer from "./components/TodoContainer"
-import "./App.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
+// component file
+import TodoContainer from './components/TodoContainer';
+import './App.css';
 
 ReactDOM.render(
-<React.StrictMode>
+  <React.StrictMode>
     <TodoContainer />
-  </React.StrictMode>, 
-document.getElementById("root")
-)
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
